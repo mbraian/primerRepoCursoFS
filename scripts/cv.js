9074bd3d7475742d4d4c365d4ea582bd39201ffb
Braian Mena <braianmena@gmail.com>
2023-02-12 19:30:44 -0300

    TP5: Finalizacion, usando los archivos ch.hmtl, cv_estilos.css y cv.js

--- scripts/cv.js
+++ scripts/cv.js
@@ -1,1 +1,20 @@
-console.log('Hola gil')
+//////--------------IMPLEMENTACION DE JS DEL TP5
+let contacto = {
+    nombreCompleto:"",
+    telefono:"",
+    mensaje:""
+};
+
+function datosContacto(){
+    contacto.nombreCompleto = `${document.getElementById("apellido").value} ${document.getElementById("nombre").value}`;
+    contacto.telefono = document.getElementById("telefono").value;
+    contacto.mensaje = document.getElementById("mensaje").value;
+    
+    if(contacto.nombre ==='' || contacto.telefono === '' || contacto.mensaje === ''){
+        alert('Debe completar todos los campos')
+    } else {
+        alert(`Mensaje enviado!\nGracias por contactarte, ${contacto.nombreCompleto}.`)
+    }
+
+    console.log(contacto)
+}
